e54b24a40f9931ece57988b27e9b00db581eb542
GENi05 <77204040+xchromium@users.noreply.github.com>
2021-01-16 14:25:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-		var mouseX = 0;
-		var mouseY = 0;
-		var popupCounter = 0;
+var mouseX = 0;
+var mouseY = 0;
+var popupCounter = 0;
 
-		document.addEventListener("mousemove", function(e) {
-			mouseX = e.clientX;
-			mouseY = e.clientY;
-			document.getElementById("coordinates").innerHTML = "<br />X: " + e.clientX + "px<br />Y: " + e.clientY + "px";
-		});
+document.addEventListener("mousemove", function(e) {
+	mouseX = e.clientX;
+	mouseY = e.clientY;
+	document.getElementById("coordinates").innerHTML = "<br />X: " + e.clientX + "px<br />Y: " + e.clientY + "px";
+});
 
-		$(document).mouseleave(function () {
-			if (mouseY < 100) {
-				if (popupCounter < 1) {
-					alert("Please do not close the tab!");
-				}
-				popupCounter ++;
-			}
-		});
+$(document).mouseleave(function () {
+	if (mouseY < 100) {
+		if (popupCounter < 1) {
+			alert("Please do not close the tab!");
+		}
+		popupCounter ++;
+	}
+});
