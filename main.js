ccfdd3d6b1b86841372df5fa7268af04a9e4db3c
GENi05 <77204040+xchromium@users.noreply.github.com>
2021-01-16 14:18:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-  
-const button = document.querySelector('.contact');
+		var mouseX = 0;
+		var mouseY = 0;
+		var popupCounter = 0;
 
-button.addEventListener('click', function(){
-    alert('Contact Me Button Clicked!')
-});
+		document.addEventListener("mousemove", function(e) {
+			mouseX = e.clientX;
+			mouseY = e.clientY;
+			document.getElementById("coordinates").innerHTML = "<br />X: " + e.clientX + "px<br />Y: " + e.clientY + "px";
+		});
+
+		$(document).mouseleave(function () {
+			if (mouseY < 100) {
+				if (popupCounter < 1) {
+					alert("Please do not close the tab!");
+				}
+				popupCounter ++;
+			}
+		});
